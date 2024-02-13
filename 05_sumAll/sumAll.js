63b906172cbcd4db46796d8bac177b0782890acc
SlubblesThuggy <65078309+SlubblesThuggy@users.noreply.github.com>
2024-02-13 20:17:13 +0100

    Completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0) { // args must be positive integers
+        return "ERROR";
+    }
 
+    if (end < start) { // swap so that start < end
+        const tmp = start;
+        start = end;
+        end = tmp;
+    }
+
+    let sum = end; // add the end first
+    for (let i = start; i < end; i++) { // then add everything else in order
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
