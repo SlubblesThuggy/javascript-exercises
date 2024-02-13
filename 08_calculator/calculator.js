5bca5c030fbc0427cb7a7602dcb250bdac4292bf
SlubblesThuggy <65078309+SlubblesThuggy@users.noreply.github.com>
2024-02-13 21:20:06 +0100

    Completed exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(lhs, rhs) {
+	return lhs + rhs;
 };
 
-const subtract = function() {
-	
+const subtract = function(lhs, rhs) {
+	return lhs - rhs;
 };
 
-const sum = function() {
-	
+const sum = function(terms) {
+	let sum = 0;
+  terms.forEach(term => sum += term);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(factors) {
+  let product = 1;
+  factors.forEach(factor => product *= factor);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return Math.pow(base, exp);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  for (let i = 2; i <= num; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
