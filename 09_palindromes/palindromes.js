16d68eaa903140ec796f880ef2b5ca79a03248e9
SlubblesThuggy <65078309+SlubblesThuggy@users.noreply.github.com>
2024-02-25 21:33:43 +0100

    Completed exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    input = input.replace(/[^A-z0-9]/g, "").toLowerCase();
+    return input.split("").reverse().join("") == input;
 };
 
 // Do not edit below this line
