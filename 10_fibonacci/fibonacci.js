0d4995801166097bb662c73b5666eeae5d68ef57
SlubblesThuggy <65078309+SlubblesThuggy@users.noreply.github.com>
2024-02-25 22:02:23 +0100

    Completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(i) {
+    if (i < 0) return "OOPS";
+    if (i == 0) return 0;
 
+    let a = 0;
+    let b = 1;
+
+    for (; i > 1; i--) {
+        let temp = b;
+        b += a;
+        a = temp;
+    }
+
+    return b;
 };
 
 // Do not edit below this line
